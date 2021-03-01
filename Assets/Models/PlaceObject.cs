e0e5f37fcd20b583d600163c8fbb3d65a6272550
Jonas Žalys <jonas.zalyz@gmail.com>
2021-03-01 20:56:14 +0200

    Adding working project

---
+++ Assets/Models/PlaceObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
