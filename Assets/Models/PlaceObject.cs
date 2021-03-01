31ec70351dc9d796692a089ccd3ff608bcbd45c0
Jonas Žalys <jonas.zalyz@gmail.com>
2021-03-01 21:57:32 +0200

    Try planes

--- Assets/Models/PlaceObject.cs
+++ Assets/Models/PlaceObject.cs
@@ -3,20 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems; 
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlaceObject : MonoBehaviour
 {
+    public GameObject placementIndicator;
+
     private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
 
     // Start is called before the first frame update
     void Start() 
     {
         arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycastManager = arOrigin.GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
     }
 }
