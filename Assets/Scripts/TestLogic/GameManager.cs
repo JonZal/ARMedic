8ef516944ea9b718389bd740e45334246373cb3b
Jonas Žalys <jonas.zalyz@gmail.com>
2021-07-28 21:55:24 +0300

    added code

---
+++ Assets/Scripts/TestLogic/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Question[] _questions = null;
+    public Question[] Questions { get { return _questions; } }
+
+    private List<int> _finishedQuestions = new List<int>(); // so we don't repeat questions
+    private int currentQuestion = 0;
+    private double score;
+
+    void Display()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TestLogic/Question.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Answer
+{
+    [SerializeField] private string _info = string.Empty;
+    public string info { get { return _info; } }
+
+    [SerializeField] private bool _isCorrect = false;
+    public bool isCorrect { get { return _isCorrect; } }
+
+}
+
+public class Question : MonoBehaviour
+{
+    public enum AnswerType { Multi, Single }
+
+    [SerializeField] private string _info = string.Empty;
+    public string info { get { return _info; } }
+
+    [SerializeField] Answer[] _answers = null;
+    public Answer[] Answers { get { return _answers; } }
+
+    // Parameters
+
+    [SerializeField] private bool _useTimer = false;
+    public bool UseTimer { get { return _useTimer; } }
+
+    [SerializeField] private int _timer = 0;
+    public int Timer { get { return _timer; } }
+
+    [SerializeField] private AnswerType _answerType = 0;
+    public AnswerType answerType { get { return _answerType; } }
+
+    [SerializeField] private double _addScore = 10;
+    public double AddScore { get { return _addScore; } }
+
+    public List<int> GetCorrectAnswers ()
+    {
+        List<int> CorrectAnswers = new List<int>();
+        for (int i = 0; i < Answers.Length; i++)
+        {
+            if(Answers[i].isCorrect)
+            {
+                CorrectAnswers.Add(i);
+            }
+        }
+        return CorrectAnswers;
+    }
+
+
+
+}
